dbbc0042d5b5b6bc3bc29328ebdb392779fc08a3
Evan Carmi <evan@ecarmi.org>
2014-12-10 13:53:12 -0800

    Remove auto install

--- modules/people/manifests/carmi.pp
+++ modules/people/manifests/carmi.pp
@@ -28,22 +28,7 @@ class people::carmi {
   include lastpass
 
   $home_dir = "/Users/${::boxen_user}"
-  $my_dir = "${boxen::config::srcdir}/my"
-  $dotfiles_dir = "${my_dir}/dotfiles"
-
-  file { $my_dir:
-    owner   => $::boxen_user,
-    group   => staff,
-    ensure  => directory,
-    recurse => true,
-  }
-
-  # boxen
-  file { "${my_dir}/boxen":
-    ensure  => link,
-    target  => $boxen::config::repodir,
-    require => File[$my_dir],
-  }
+  $dotfiles_dir = "${home_dir}/dotfiles"
 
   # dotfiles
   repository { $people::carmi::dotfiles_dir:
@@ -51,11 +36,4 @@ class people::carmi {
     require => File[$people::carmi::my_dir],
   }
 
-  exec { "install dotfiles":
-    provider => shell,
-    command  => "./script/bootstrap",
-    cwd      => $people::carmi::dotfiles_dir,
-    creates  => "${people::carmi::home_dir}/.vimrc",
-    require  => Repository[$people::carmi::dotfiles_dir],
-  }
 }
