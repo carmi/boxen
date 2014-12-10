c5174687c57e14e04a737e1851698bb4627e8758
Evan Carmi <evan@ecarmi.org>
2014-12-10 14:29:27 -0800

    ADd dotfiles

--- modules/people/manifests/carmi.pp
+++ modules/people/manifests/carmi.pp
@@ -26,4 +26,29 @@ class people::carmi {
   include ohmyzsh
 
   include lastpass
+
+  $home_dir = "/Users/${::boxen_user}"
+  $my_dir = "${boxen::config::srcdir}/my"
+  $dotfiles_dir = "${my_dir}/dotfiles"
+
+  file { $my_dir:
+    owner   => $::boxen_user,
+    group   => staff,
+    ensure  => directory,
+    recurse => true,
+  }
+
+  # dotfiles
+  repository { $people::carmi::dotfiles_dir:
+    source => "${::github_login}/dotfiles-1",
+    require => File[$people::carmi::my_dir],
+  }
+
+  #exec { "install dotfiles":
+  #  provider => shell,
+  #  command  => "./script/bootstrap",
+  #  cwd      => $people::carmi::dotfiles_dir,
+  #  creates  => "${people::carmi::home_dir}/.vimrc",
+  #  require  => Repository[$people::carmi::dotfiles_dir],
+  #}
 }
